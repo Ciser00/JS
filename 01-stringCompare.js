8c200511ffac223925a83c8e17613264e331b3e0
ciser00 <gpc281@nyu.edu>
2020-04-02 14:47:23 -0500

    save

---
+++ 01-stringCompare.js
@@ -0,0 +1,61 @@
+/*
+var best_pet = "yes";
+
+
+if (best_pet == "dog"){
+  alert("you are correct!")
+}
+else{
+  alert("you are wrong!")
+}
+*/
+
+
+/*
+var num =9;
+
+if (num == 10){
+  alert("Good Choice!")
+}else if (num > 10){
+  alert("Too High")
+}else{
+  alert("Too Low")
+}
+*/
+
+/*
+function speaker(string1, string2){
+  return(string1 + " " + string2)
+}
+console.log(speaker("You're", "Cool"));
+*/
+
+/*
+function paths(direction){
+  if (direction == "north"){
+    console.log("You come across a vast and green field")
+  }
+  else if (direction == "east"){
+    console.log("A cold tundra awaits you");
+  }
+  else if (direction == "south"){
+    console.log("You are blasted by the heat of a desert");
+  }
+  else{
+    console.log("pick a valid direction");
+  }
+}
+paths("north")
+paths("east")
+paths("south")
+paths("esouth")
+*/
+/*
+var array1 = ["Hi","Hello","What's up"];
+var array2 = ["you","he","she"];
+var array3 = ["smells","is cool"];
+var word1 = Math.floor( Math.random()*array1.length);
+var word2 = Math.floor( Math.random()*array2.length);
+var word3 = Math.floor( Math.random()*array3.length);
+console.log(array1[word1], array2[word2], array3[word3]);
+*/
---
+++ 02-numberCompare.js
@@ -0,0 +1,9 @@
+var num =10;
+
+if (num == 10){
+  alert("Good Choice!")
+}if else (num > 10){
+  alert("Too High")
+}else{
+  alert("Too Low")
+}
